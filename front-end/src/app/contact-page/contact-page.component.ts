9546c4dc8d0a04541cc7db6a8aa6b91f184c9df7
gianmarialessandro <moretti@gianmarialessandro.com>
2022-05-20 00:26:09 +0200

    "addding componet funcionality"

--- front-end/src/app/contact-page/contact-page.component.ts
+++ front-end/src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { ListingDetailPageComponent } from '../listing-detail-page/listing-detail-page.component';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  email: string = '';
+  message: string = '';
+  listing: Listing;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+    this.message = `Hi, i'm interested in your ${this.listing.name.toLowerCase()}!`;
+  }
+
+  sendMessage(): void {
+    alert('Your message has been sent');
+    this.router.navigateByUrl('/listings');
   }
 
 }
